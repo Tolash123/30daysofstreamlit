1139afc1768697b3270fe08cfe466356e75e309f
Tolash123 <idreesy31@gmail.com>
2024-01-13 06:37:21 +0100

    day18

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
 import streamlit as st
 
-# Everything is accessible via the st.secrets dict:
+st.title('st.secrets')
 
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
-
-# And the root-level secrets are also accessible as environment variables:
-
-import os
-st.write(
-	"Has environment variables been set:",
-	os.environ["db_username"] == st.secrets["db_username"])
+st.write(st.secrets['message'])
